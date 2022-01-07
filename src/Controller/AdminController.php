bdb0f5a494cbe7dca068f3ef23c4c83e51002998
Pzon <piotrzon@gmail.com>
2022-01-07 15:27:28 +0100

    chptr 125 done workinf features for admin panel part2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -19,4 +19,44 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/editCategory", name="editCategory")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos()
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/uploadVideo", name="uploadVideo")
+     */
+    public function uploadVideo()
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
 }
