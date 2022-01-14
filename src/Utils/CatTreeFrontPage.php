c9d316eb05843b316c7212edfe1fa8aaa5fc4b4a
Pzon <piotrzon@gmail.com>
2022-01-14 15:14:31 +0100

    chptr133&134 done

---
+++ src/Utils/CatTreeFrontPage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utils;
+
+use App\Utils\AbstractClasses\CatTreeAbstract;
+
+class CatTreeFrontPage extends CatTreeAbstract{
+	public function getCategoryList(array $catArray){
+			//to do;
+	}
+}
