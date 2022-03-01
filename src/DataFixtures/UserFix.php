687debc73ad8d1efdf9820e2f57424540d2f4371
Pzon <piotrzon@gmail.com>
2022-03-01 15:21:03 +0100

    163&164 done loaded users fixtures and edited security.yaml file

---
+++ src/DataFixtures/UserFix.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFix extends Fixture
+{
+
+    public function __construct(UserPasswordEncoderInterface $password_encoder)
+    {
+        $this->password_encoder = $password_encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$name, $last_name, $email, $password, $api_key, $roles]){
+            $user = new User();
+            $user->setName($name);
+            $user->setLastName($last_name);
+            $user->setEmail($email);
+            $user->setPassword($this->password_encoder->encodePassword($user, $password));
+            $user->setVimeoApiKey($api_key);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    private function getUserData(): array{
+        return [
+
+            ['John', 'Wayne', 'pz@pzon.eu', 'pass', 'hjd8dehdh', ['ROLE_ADMIN']],
+            ['John', 'Wayne2', 'pz2@pzon.eu', 'pass', null, ['ROLE_ADMIN']],
+            ['John', 'Doe', 'pz3@pzon.eu', 'pass', null, ['ROLE_USER']]
+
+        ];
+    }
+}
