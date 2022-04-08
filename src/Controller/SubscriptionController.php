f276016264a05a6b61bcfbd52bf0ac22717ae4ba
Pzon <piotrzon@gmail.com>
2022-04-08 15:03:14 +0200

    chptr212-215 done; features subscription process done

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -7,11 +7,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Entity\Subscription;
+use App\Controller\Traits\SaveSubscription;
 
 
 class SubscriptionController extends AbstractController
 {
-
+    use SaveSubscription;
     /**
      * @Route("/pricing", name="pricing")
      */
@@ -21,6 +22,19 @@ class SubscriptionController extends AbstractController
             'name' => Subscription::getPlanDataNames(),
             'price' => Subscription::getPlanDataPrice(),
         ]);
+    }
+
+     /**
+     * @Route("/payment/{paypal}", name="payment", defaults = {"paypal":false})
+     */
+    public function payment($paypal, SessionInterface $session){
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        if($paypal){
+            $this->saveSubscription($session->get('planName'), $this->getUser());
+            return $this->redirectToRoute('adminPage');
+        }
+        return $this->render('front/payment.html.twig');
     }
 
 }
