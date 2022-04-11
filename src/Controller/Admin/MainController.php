f6c6fe1441e89c7dfe4a98b0f3c9cfcec731b18d
Pzon <piotrzon@gmail.com>
2022-04-11 14:26:29 +0200

    chptr216 done, features:delete admin acc

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -68,4 +68,18 @@ class MainController extends AbstractController{
 
         return $this->redirectToRoute('adminPage');
     }
+
+    /**
+     * @Route("/deleteAccount", name="deleteAccount")
+     */
+    public function deleteAccount(){
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->find($this->getUser());
+        $em->remove($user);
+        $em->flush();
+
+        session_destroy();
+
+        return $this->redirectToRoute('main_page');
+    }
 }
